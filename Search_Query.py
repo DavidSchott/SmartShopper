da75b20dfec1849bb41e69d285f06fd42e36a118
DavidSchott <s1329380@sms.ed.ac.uk>
2015-04-04 20:32:40 +0100

    Changed the Location values of the XML-file and added filtering by location to Search_Query

---
+++ Search_Query.py
@@ -0,0 +1,46 @@
+__author__ = 'David'
+from xml.dom.minidom import parse
+import xml.dom.minidom
+from xml.etree.ElementTree import ElementTree as ET
+from geopy.distance import vincenty as vincent
+class Search_Query:
+    def __init__(self,product_node,radius,price_range,output_length):
+        self.product_node = product_node
+        self.radius = radius
+        self.price_range = price_range
+        self.output_length = output_length
+
+
+#Example used as template
+#DOMTree = xml.dom.minidom.parse("Stores.xml")
+collection = ET()
+root = collection.parse("Stores.xml")
+product_node = root[0][0][0] # <PRODUCT PRICE="1" NAME="black chair" GEOID="125665" URL="http://www.buyme.com/product=125665"></PRODUCT>
+radius = 500
+store_location = "(41.49008, -71.312796)"
+product_category = "Furniture"
+price_range = 50
+output_length = 3
+
+#Returns co-ords
+def getCoords(co_ord):
+    co_ord_list = co_ord.split(",")
+    x = float(co_ord_list[0][1:])
+    y = float(co_ord_list[1][:-1])
+    return [x,y]
+"""Filters the stores by location"""
+def filterLocation():
+    for store in root.findall("STORE"):
+        dist = vincent(getCoords(store.get("LOCATION")),getCoords(store_location)).meters
+        if (dist > radius):
+            root.remove(store)
+    return root
+
+
+def main():
+    product_node = root[0][0][0]
+    subcollection = ET(filterLocation())
+    subcollection.write('newpred.xml', xml_declaration=True)
+
+
+
